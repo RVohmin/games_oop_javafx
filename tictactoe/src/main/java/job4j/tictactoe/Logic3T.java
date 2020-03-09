523ff1508975b78cbc16103e17f847ae95da2357
RVohmin <vohmin77@gmail.com>
2020-03-09 15:48:54 +0300

    Крестики-нолики на JavaFX[#234906]

--- tictactoe/src/main/java/job4j/tictactoe/Logic3T.java
+++ tictactoe/src/main/java/job4j/tictactoe/Logic3T.java
@@ -1,6 +1,10 @@
 package job4j.tictactoe;
 
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Logic3T {
     private final Figure3T[][] table;
@@ -24,56 +28,46 @@ public class Logic3T {
     }
 
     public boolean isWinnerX() {
+        if (this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 1) ||
+                this.fillBy(Figure3T::hasMarkX, this.table.length - 1, 0, -1, 1)) {
+            return true;
+        }
         for (int i = 0; i < this.table.length; i++) {
             if (this.fillBy(Figure3T::hasMarkX, 0, i, 1, 0) ||
-                    this.fillBy(Figure3T::hasMarkX, i, 0, 0, 1) ||
-                    this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 1) ||
-                    this.fillBy(Figure3T::hasMarkX, this.table.length - 1, 0, -1, 1)
+                    this.fillBy(Figure3T::hasMarkX, i, 0, 0, 1)
             ) {
                 return true;
             }
         }
         return false;
     }
-//         return this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 0) || //вертикаль слева
-//                this.fillBy(Figure3T::hasMarkX, 0, 1, 1, 0) || //вертикаль средняя
-//                this.fillBy(Figure3T::hasMarkX, 0, 2, 1, 0) || //вертикаль справа
-//                this.fillBy(Figure3T::hasMarkX, 0, 0, 0, 1) || //горизонталь сверху
-//                this.fillBy(Figure3T::hasMarkX, 1, 0, 0, 1) || //горизонталь средняя
-//                this.fillBy(Figure3T::hasMarkX, 2, 0, 0, 1) || //горизонталь нижняя
-//                this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 1) || // диагональ слева верха
-//                this.fillBy(Figure3T::hasMarkX, this.table.length - 1, 0, -1, 1); // диагональ справа сверху
-
 
     public boolean isWinnerO() {
+        if (this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 1) ||
+                this.fillBy(Figure3T::hasMarkO, this.table.length - 1, 0, -1, 1)) {
+            return true;
+        }
         for (int i = 0; i < this.table.length; i++) {
             if (this.fillBy(Figure3T::hasMarkO, 0, i, 1, 0) ||
-                    this.fillBy(Figure3T::hasMarkO, i, 0, 0, 1) ||
-                    this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 1) ||
-                    this.fillBy(Figure3T::hasMarkO, this.table.length - 1, 0, -1, 1)
+                    this.fillBy(Figure3T::hasMarkO, i, 0, 0, 1)
             ) {
                 return true;
             }
         }
         return false;
     }
-//        return this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 0) || //вертикаль слева
-//                this.fillBy(Figure3T::hasMarkO, 0, 0, 0, 1) || //горизонталь сверху
-//                this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 1) || //диагональ слева сверху
-//                this.fillBy(Figure3T::hasMarkO, 0, 1, 1, 0) || //вертикаль средняя
-//                this.fillBy(Figure3T::hasMarkO, 0, 2, 1, 0) || //вертикаль справа
-//                this.fillBy(Figure3T::hasMarkO, 1, 0, 0, 1) || //горизонталь средняя
-//                this.fillBy(Figure3T::hasMarkO, 2, 0, 0, 1) || //горизонталь нижняя
-//                this.fillBy(Figure3T::hasMarkO, this.table.length - 1, 0, -1, 1); //диагональ справа сверху
 
     public boolean hasGap() {
-        for (int i = 0; i < table.length; i++) {
-            for (int j = 0; j < this.table.length; j++) {
-                if (!table[i][j].hasMarkX() && !table[i][j].hasMarkO()) {
-                    return true;
-                }
-            }
-        }
-        return false;
+        return Arrays.stream(this.table)
+                .flatMap(Arrays::stream)
+                .anyMatch(e -> !e.hasMarkX() && !e.hasMarkO());
     }
+//        for (int i = 0; i < table.length; i++) {
+//            for (int j = 0; j < this.table.length; j++) {
+//                if (!table[i][j].hasMarkX() && !table[i][j].hasMarkO()) {
+//                    return true;
+//                }
+//            }
+//        }
+//        return false;
 }
